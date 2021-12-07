3c0f6cbff9bc8c7ad242abb0e55db98080e0be0c
srchd <sipirichi@gmail.com>
2021-12-08 00:57:39 +0100

    Started Day 4 part two

--- 4/bingo.py
+++ 4/bingo.py
@@ -1,5 +1,4 @@
 import numpy as np
-from copy import deepcopy
 
 with open('input.txt', 'r') as file:
     numbers = file.readline()
@@ -36,6 +35,8 @@ for board in temp_board:
 
 np_board = np.array(final_boards)
 
+# Gross solution, part one:
+
 i_len = len(np_board)
 j_len = len(np_board[0])
 k_len = len(np_board[0][0])
@@ -65,4 +66,29 @@ for number in numbers:
 
 result *= sum(sum(np_board[winner_board]))
 
-print(result)
+print(f'Part One: {result}')
+
+# part two
+
+result = 0
+winner_board = 0
+for number in numbers:
+    for i in range(i_len):
+        for j in range(j_len):
+            for k in range(k_len):
+                if np_board[i][j][k] == number:
+                    np_board[i][j][k] = 0
+    rows_per_board = np.sum(np_board, axis=2)
+    columns_per_board = np.sum(np_board, axis=1)
+    possible_rows = np.where(rows_per_board == 0)
+    possible_cols = np.where(columns_per_board == 0)
+    if len(possible_rows[0]) != 0 and len(possible_rows[1]) != 0:
+        winner_board = possible_rows[0][0]
+        result = number
+        break
+    elif len(possible_cols[0]) != 0 and len(possible_cols[1]) != 0:
+        winner_board = possible_cols[0][0]
+        result = number
+        break
+    else:
+        pass
